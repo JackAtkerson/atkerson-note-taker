511b4d9f45cae7d97359b3a6f5f508efec9de08a
Jack Atkerson <jatkerson18@gmail.com>
2022-07-03 12:31:11 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,10 +1,60 @@
 const express = require('express');
 const notes = require ('./db/db.json');
+const fs = require ('fs');
+const path = require ('path');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-function filterByQuery(query, notesArray) {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('punlic'));
+app.use(express.json());
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/dc.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+        let userNote = req.body;
+        userNote.id = Math.floor(Math.random() * 5000);
+        notes.push(userNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+    });
+    });
+});
+
+app.delete('/api/notes:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
+    
+    fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
+        res.json({msg: 'successfully'});
+    });
+    });
+});
+
+app.get('api/notes/:id', (req, res) => {
+    res.json(notes[req.params.id]);
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+/*function filterByQuery(query, notesArray) {
     let filteredResults = notesArray;
     if (query.title) {
         filteredResults = filteredResults.filter(note => note.title === query.title);
@@ -15,6 +65,16 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 };
 
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
@@ -24,10 +84,13 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
-});
+    req.body.id = notes.length.toString();
 
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+});
+*/
 app.listen(PORT, () => {
     console.log`API Server now on port ${PORT}!`;
 });
